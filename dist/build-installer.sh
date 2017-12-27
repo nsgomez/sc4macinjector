b95e2abe9e47edb35e65b84a2e42af0cd31a98d2
Nelson Gomez <nelson.gomez.msd@gmail.com>
2017-12-26 20:34:52 -0500

    Bump installer version

--- dist/build-installer.sh
+++ dist/build-installer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pkgbuild --identifier me.ngomez.sc4.MacInjector --root Root --version "0.9.0" --scripts scripts --install-location / me.ngomez.sc4.MacInjector.install.pkg
+pkgbuild --identifier me.ngomez.sc4.MacInjector --root Root --version "0.9.1" --scripts scripts --install-location / me.ngomez.sc4.MacInjector.install.pkg
 productbuild --distribution distribution.xml --resources resources/ "SC4MacInjector.pkg"
